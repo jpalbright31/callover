e06986bf394264edc61fa2c73e6d2c80f204d8a6
Louis De-Campou <lpl.ldecampou@macandrews.net>
2017-04-26 11:10:03 +0100

    commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using MVC5T_CallOver.DAL.CallOver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5T_CallOver.ViewModels;
+
+namespace MVC5T_CallOver.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Indexv2()
+        {
+            return View();
+        }
+
+        public ActionResult Loader()
+        {
+            return View();
+        }
+
+        public ActionResult Containers_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            var db = new CallOverContext();
+
+            var model = db.Containers;
+
+            DataSourceResult result = model.ToDataSourceResult(request, ModelState, m => new vm_Container()
+            {
+                ID = m.ID,
+                ContainerNo = m.ContainerNo,
+                Type = m.Type,
+                VoyageNo = m.VoyageNo,
+                Port = m.Port,
+                HaulierAC = m.HaulierAC,
+                HaulierName = m.HaulierName,
+                DestinationOrigin = m.DestinationOrigin,
+                dbBookingDate = m.BookingDate,//(m.BookingDate.HasValue) ? m.BookingDate.Value : new DateTime(),
+                dbBookingTime = m.BookingTime,//(m.BookingTime.HasValue) ? m.BookingTime.Value : new TimeSpan(),
+                dbGateDate = m.GateDate,//(m.GateDate.HasValue) ? m.GateDate.Value : new DateTime(),
+                dbGateTime = m.GateTime//(m.GateTime.HasValue) ? m.GateTime.Value : new TimeSpan()
+            });
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Excel_Export_Save(string contentType, string base64, string fileName)
+        {
+            var fileContents = Convert.FromBase64String(base64);
+
+            return File(fileContents, contentType, fileName);
+        }
+
+        [HttpPost]
+        public ActionResult Pdf_Export_Save(string contentType, string base64, string fileName)
+        {
+            var fileContents = Convert.FromBase64String(base64);
+
+            return File(fileContents, "application/pdf", fileName);//return File(fileContents, contentType, fileName);
+        }
+
+        //public ActionResult TestInfo_Read([DataSourceRequest]DataSourceRequest request)
+        //{
+        //    var context = MvcApplication.MacDocsSession;
+
+        //    var model = context.Query<CargoCrControlCnum>();
+
+        //    DataSourceResult result = model.AsQueryable().ToDataSourceResult(request, ModelState, m => new vm_CargoControl()
+        //    {
+        //        Location = m.Location,
+        //        VoyageNo = m.VoyageNo,
+        //        Direction = m.Direction,
+        //        BaseFrom = m.BaseFrom,
+        //        BaseTo = m.BaseTo,
+        //        BillNo = m.BillNo,
+        //        Account = m.Account,
+        //        Name = m.Name,
+        //        ContainerNo = m.ContainerNo,
+        //        ContainerType = m.ContainerType,
+        //        ContainerLength = m.ContainerLength
+        //    });
+
+        //    return Json(result, JsonRequestBehavior.AllowGet);
+        //}
+    }
+}
---
+++ ViewModels/vm_Container.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+
+namespace MVC5T_CallOver.ViewModels
+{
+    public class vm_Container {
+
+        [Key]
+        [ScaffoldColumn(false)]
+        public int ID { get; set; }
+
+        [Display(Name = "Container No")]
+        public string ContainerNo { get; set; }
+
+        public string Type { get; set; }
+
+        [Display(Name = "Voyage No")]
+        public string VoyageNo { get; set; }
+
+        public string Port { get; set; }
+
+        [Display(Name = "Haulier A/C")]
+        public string HaulierAC { get; set; }
+
+        [Display(Name = "Haulier Name")]
+        public string HaulierName { get; set; }
+
+        [Display(Name = "Dest/Origin")]
+        public string DestinationOrigin { get; set; }
+
+        [Display(Name = "Booking Date")]
+        public DateTime? BookingDate
+        {
+            get
+            {
+				return dbBookingDate ?? null;
+            }
+        }
+
+        public DateTime? BookingTime
+        {
+            get
+            {
+                if (dbBookingDate.HasValue && dbBookingTime.HasValue)
+                {
+                    return new DateTime(1, 1, 1, dbBookingTime.Value.Hours, dbBookingTime.Value.Minutes, dbBookingTime.Value.Seconds);
+                }
+                if (dbBookingDate.HasValue && !dbBookingTime.HasValue)
+                {
+                    return new DateTime(1, 1, 1, dbBookingTime.Value.Hours, dbBookingTime.Value.Minutes, dbBookingTime.Value.Seconds);
+                }
+                if (!dbBookingDate.HasValue && dbBookingTime.HasValue)
+                {
+                    return new DateTime(1, 1, 1, dbBookingTime.Value.Hours, dbBookingTime.Value.Minutes, dbBookingTime.Value.Seconds);
+                }
+
+                return null;
+            }
+        }
+
+        [Display(Name = "Gate Date")]
+        public DateTime? GateDate
+        {
+            get
+            {
+                return dbGateDate ?? null;
+            }
+        }
+
+        public DateTime? GateTime
+        {
+            get
+            {
+                if (dbGateDate.HasValue && dbGateTime.HasValue)
+                {
+                    return new DateTime(1, 1, 1, dbGateTime.Value.Hours, dbGateTime.Value.Minutes, dbGateTime.Value.Seconds);
+                }
+                if (dbGateDate.HasValue && !dbGateTime.HasValue)
+                {
+                    return new DateTime(1, 1, 1, dbGateTime.Value.Hours, dbGateTime.Value.Minutes, dbGateTime.Value.Seconds);
+                }
+                if (!dbGateDate.HasValue && dbGateTime.HasValue)
+                {
+                    return new DateTime(1, 1, 1, dbGateTime.Value.Hours, dbGateTime.Value.Minutes, dbGateTime.Value.Seconds);
+                }
+
+                return null;
+            }
+        }
+
+        [ScaffoldColumn(false)]
+        public DateTime? dbBookingDate { get; set; }
+
+        [ScaffoldColumn(false)]
+        public TimeSpan? dbBookingTime { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime? dbGateDate { get; set; }
+
+        [ScaffoldColumn(false)]
+        public TimeSpan? dbGateTime { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string DateCheck
+        {
+            get
+            {
+                if (GateDate.HasValue && BookingDate.HasValue)
+                {
+                    if (GateDate.Value.Date > BookingDate.Value.Date)
+                        return "OFF DATE";
+                    else
+                        return "ON DATE";
+
+                }
+                else
+                {
+                    return "CHECK";
+                }
+            }
+        }
+
+        [ScaffoldColumn(false)]
+        public string Band1
+        {
+            get
+            {
+                if(GateDate.HasValue && BookingDate.HasValue)
+                {
+                    var tempGateDate = GateDate.Value;
+                    var check = tempGateDate.AddHours(3);
+
+                    if ((check.Date) < BookingDate.Value.Date && check.TimeOfDay < BookingDate.Value.TimeOfDay)
+                    {
+                        return "ON TIME";
+                    }
+                    else
+                    {
+                        return "CHECK 1";
+                    }
+                }
+                else
+                {
+                    return "CHECK 1";
+                }
+            }
+        }
+
+        [ScaffoldColumn(false)]
+        public string Band2
+        {
+            get
+            {
+                if (GateDate.HasValue && BookingDate.HasValue)
+                {
+                    var tempGateDate = GateDate.Value;
+                    var check = tempGateDate.AddHours(2);
+
+                    if (check.Date < BookingDate.Value.Date && check.TimeOfDay < BookingDate.Value.TimeOfDay)
+                    {
+                        return "ON TIME";
+                    }
+                    else
+                    {
+                        return "CHECK 2";
+                    }
+                }
+                else
+                {
+                    return "CHECK 2";
+                }
+            }
+        }
+
+        [ScaffoldColumn(false)]
+        public string Band3
+        {
+            get
+            {
+                if (GateDate.HasValue && BookingDate.HasValue)
+                {
+                    var tempGateDate = GateDate.Value;
+                    var check = tempGateDate.AddHours(1);
+
+                    if (check.Date < BookingDate.Value.Date && check.TimeOfDay < BookingDate.Value.TimeOfDay)
+                    {
+                        return "ON TIME";
+                    }
+                    else
+                    {
+                        return "CHECK 3";
+                    }
+                }
+                else
+                {
+                    return "CHECK 3";
+                }
+            }
+        }
+
+        [ScaffoldColumn(false)]
+        public string Band4
+        {
+            get
+            {
+                if (GateDate.HasValue && BookingDate.HasValue)
+                {
+                    if (GateDate.Value < BookingDate.Value)
+                    {
+                        return "ON TIME";
+                    }
+                    else
+                    {
+                        return "LATE";
+                    }
+                }
+                else
+                {
+                    return "CHECK 4";
+                }
+            }
+        }
+    }
+
+}
